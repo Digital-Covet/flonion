5147de6944012fd33f1cab0014207638a43d3771
Atharva Thakur <atharva.thakur@digitalcovet.com>
2026-08-06 11:04:47 +0530

    style(auth): increase brandmark logo size from h-6 to h-14

--- src/components/auth/brandmark.tsx
+++ src/components/auth/brandmark.tsx
@@ -2,6 +2,6 @@ import type { Component } from 'solid-js';
 import LogoMark from "@/assets/logomark"
 export const BrandMark: Component = () => (
   <div class="mb-14">
-    <LogoMark class='h-6' />
+    <LogoMark class='h-14' />
   </div>
 );
